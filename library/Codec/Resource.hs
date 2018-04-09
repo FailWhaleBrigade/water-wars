ed1ff4ff6bff0d444d981222f8861c1522ee7ed3
fendor <power.walross@gmail.com>
2018-04-09 15:05:39 +0200

    Improve error handling in loadPngAsBmp

--- library/Codec/Resource.hs
+++ library/Codec/Resource.hs
@@ -2,19 +2,20 @@ module Codec.Resource where
 
 import ClassyPrelude
 import Graphics.Gloss
-import Graphics.Gloss.Data.Bitmap
 import Codec.Picture
 
 loadPngAsBmp :: FilePath -> IO (Either String Picture)
 loadPngAsBmp fp = do
-    Right png <- readPng fp
-    let Right bmp = encodeDynamicBitmap png
+    pngEither <- readPng fp
     return 
-        $ Right 
-            (bitmapOfByteString 
-                1920 
-                1080 
-                (BitmapFormat TopToBottom PxRGBA) 
-                (toStrict bmp) 
-                True
-            )
+        $ do 
+            png <- pngEither
+            bmp <- encodeDynamicBitmap png
+            Right 
+                (bitmapOfByteString 
+                    1920 
+                    1080 
+                    (BitmapFormat TopToBottom PxRGBA) 
+                    (toStrict bmp) 
+                    True
+                )
