6fcde8d8cff20beabefb214f85e2d0e1335ae9fb
fendor <power.walross@gmail.com>
2018-04-11 17:25:52 +0200

    Fix conversion from png to bmp

--- library/Render/Resources/Tiles.hs
+++ library/Render/Resources/Tiles.hs
@@ -46,19 +46,19 @@ loadTileMap = do
         textures <- loadedTextures
         Right (mapFromList (zip [Floor .. Ceil] (toList textures)))
 
-tiles :: Seq (FilePath, Int, Int)
+tiles :: Seq FilePath
 tiles =
     fromList
-        [ ({- Floor,-} "resources/textures/block/block32.png", 32, 32)
-        , ({- EndLeft,-} "resources/textures/block/blockendleft32.png", 32, 32)
-        , ({- EndRight,-} "resources/textures/block/blockendright32.png", 32, 32)
-        , ({- BottomLeftCorner,-} "resources/textures/block/bottomleftcornerblock32.png", 32, 32)
-        , ({- BottomRightCorner,-} "resources/textures/block/bottomrightcornerblock32.png", 32, 32)
-        , ({- TopRightCorner,-} "resources/textures/block/toprightcornerblock32.png", 32, 32)
-        , ({- TopLeftCorner,-} "resources/textures/block/topleftcornerblock32.png", 32, 32)
-        , ({- LeftWall,-} "resources/textures/block/leftwallblock32.png", 32, 32)
-        , ({- RightWall,-} "resources/textures/block/rightwallblock32.png", 32, 32)
-        , ({- Middle,-} "resources/textures/block/middleblock32.png", 32, 32)
-        , ({- Ceil,-} "resources/textures/block/topblock32.png", 32, 32)
+        [ {- Floor,-} "resources/textures/block/block32.png"
+        , {- EndLeft,-} "resources/textures/block/blockendleft32.png"
+        , {- EndRight,-} "resources/textures/block/blockendright32.png"
+        , {- BottomLeftCorner,-} "resources/textures/block/bottomleftcornerblock32.png"
+        , {- BottomRightCorner,-} "resources/textures/block/bottomrightcornerblock32.png"
+        , {- TopRightCorner,-} "resources/textures/block/toprightcornerblock32.png"
+        , {- TopLeftCorner,-} "resources/textures/block/topleftcornerblock32.png"
+        , {- LeftWall,-} "resources/textures/block/leftwallblock32.png"
+        , {- RightWall,-} "resources/textures/block/rightwallblock32.png"
+        , {- Middle,-} "resources/textures/block/middleblock32.png"
+        , {- Ceil,-} "resources/textures/block/topblock32.png"
         ]
     
