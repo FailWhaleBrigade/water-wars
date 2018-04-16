606a92de7650e2221420bdf4fa51a435bc5a6bc8
Jana <e1528032@student.tuwien.ac.at>
2018-04-16 16:52:34 +0200

    Create connection function

--- library/WaterWars/Client/Network/Connection.hs
+++ library/WaterWars/Client/Network/Connection.hs
@@ -2,9 +2,18 @@ module WaterWars.Client.Network.Connection where
 
 import ClassyPrelude
 
+import Network
 import WaterWars.Client.Render.State
 
 connectionThread :: MonadIO m => NetworkConfig -> WorldSTM -> m ()
-connectionThread _ _ = return ()
+connectionThread NetworkConfig {..} world =
+    liftIO $ bracket (connectTo hostName portId) (communicate world) (hClose)
+
+communicate :: MonadIO m => WorldSTM -> Handle -> m ()
+communicate _ _ = return ()
 
 data NetworkConfig = NetworkConfig
+    { portId   :: PortID
+    , hostName :: HostName
+    } deriving (Show, Eq)
+
