4da1a7e346922542364bd31ba20d04073a8e7a59
fendor <power.walross@gmail.com>
2018-04-16 18:13:06 +0200

    Add logging framework

--- library/WaterWars/Client/Network/Connection.hs
+++ library/WaterWars/Client/Network/Connection.hs
@@ -2,24 +2,35 @@ module WaterWars.Client.Network.Connection where
 
 import ClassyPrelude
 import Network
+import System.Log.Logger
+
 import WaterWars.Client.Render.State
-import WaterWars.Core.GameState (gameTerrain) 
+import qualified WaterWars.Core.GameState as CoreState
 
 connectionThread :: MonadIO m => NetworkConfig -> WorldSTM -> m ()
 connectionThread NetworkConfig {..} world =
     liftIO $ bracket (connectTo hostName portId) hClose (communicate world)
 
 communicate :: MonadIO m => WorldSTM -> Handle -> m ()
-communicate (WorldSTM tvar) h = do
+communicate (WorldSTM tvar) h = forever $ do
     bs <- liftIO $ hGetContents h
-    let gameMapMaybe = readMay $ decodeUtf8 bs
-    case gameMapMaybe of
-        Nothing -> putStrLn "Include logging"
-        Just gameMap -> atomically $ do 
+    let maybeGameInfo = readMay $ decodeUtf8 bs
+    case maybeGameInfo of
+        Nothing ->
+            liftIO
+                .  warningM "Server Connection"
+                $  "Could not parse the gameInfo: "
+                ++ show bs
+        Just info -> atomically $ do
             world <- readTVar tvar
-            let world' = setTerrain (blockMap world) (gameTerrain gameMap) world
+            let world' = updateWorld info world
             writeTVar tvar world'
     return ()
+
+updateWorld :: CoreState.GameInformation -> World -> World
+updateWorld (CoreState.Map gameMap) world@World {..} =
+    setTerrain blockMap (CoreState.gameTerrain gameMap) world
+updateWorld (CoreState.State _) world@World {..} = world
 
 data NetworkConfig = NetworkConfig
     { portId   :: PortID
--- server/Main.hs
+++ server/Main.hs
@@ -3,13 +3,14 @@ module Main where
 import ClassyPrelude
 import Network
 import WaterWars.Core.DefaultGame
+import WaterWars.Core.GameState
 
 main :: IO ()
 main = do
   socket <- listenOn $ PortNumber 1234
   (connHandle, clientName, clientPort) <- accept socket
   putStrLn $ tshow clientName ++ " " ++ tshow clientPort
-  hPutText connHandle $ tshow defaultGameMap
+  hPutText connHandle . tshow $ Map defaultGameMap
   hClose connHandle
   sClose socket
   return ()
