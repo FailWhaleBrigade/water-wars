eb0a81bfa8866e510d0d068470005fdcc7599162
fendor <power.walross@gmail.com>
2018-04-16 19:12:34 +0200

    Add possibility to send actions

--- library/WaterWars/Client/Network/Connection.hs
+++ library/WaterWars/Client/Network/Connection.hs
@@ -4,9 +4,12 @@ import ClassyPrelude
 import Network
 import System.Log.Logger
 
+import Control.Concurrent
+
 import WaterWars.Client.Render.State (setTerrain, WorldSTM(..), World(..))
 import WaterWars.Client.Network.State (NetworkConfig(..), NetworkInfo(..))
 import qualified WaterWars.Core.GameState as CoreState
+import qualified WaterWars.Core.GameAction as CoreAction
 
 connectionThread
     :: MonadIO m => Maybe NetworkInfo -> NetworkConfig -> WorldSTM -> m ()
@@ -16,13 +19,15 @@ connectionThread _ config@NetworkConfig {..} world = liftIO $ bracket
         connectTo hostName portId
     )
     (\h -> do
-        warningM "Server Connection" "Connection has been closed"
+        warningM "Server Connection" "Connection is closed now"
         hClose h
     )
-    (communicate world)
+    -- TODO: this function swallows things
+    (\h -> race_ (recieveUpdates world h) (sendUpdates world h))
 
-communicate :: MonadIO m => WorldSTM -> Handle -> m ()
-communicate (WorldSTM tvar) h = forever $ do
+
+recieveUpdates :: MonadIO m => WorldSTM -> Handle -> m ()
+recieveUpdates (WorldSTM tvar) h = forever $ do
     bs <- liftIO $ hGetContents h
     let maybeGameInfo = readMay $ decodeUtf8 bs
     case maybeGameInfo of
@@ -33,7 +38,7 @@ communicate (WorldSTM tvar) h = forever $ do
                 ++ show bs
 
         Just info -> do
-            liftIO $ infoM "Server Connection" "Received a game update"
+            liftIO $ warningM "Server Connection" "Received a game update"
             world <- readTVarIO tvar
             let world' = updateWorld info world
             atomically $ writeTVar tvar world'
@@ -44,3 +49,19 @@ updateWorld (CoreState.Map gameMap) world@World {..} =
     setTerrain blockMap (CoreState.gameTerrain gameMap) world
 
 updateWorld (CoreState.State _) world@World {..} = world
+
+sendUpdates :: MonadIO m => WorldSTM -> Handle -> m ()
+sendUpdates (WorldSTM tvar) h = forever $ do
+    -- TODO: move this to bottom
+    liftIO $ threadDelay (seconds 1.0)
+    liftIO $ warningM "Server Connection" "Send an update to the Server"
+    world <- readTVarIO tvar
+    let action = extractGameAction world
+    hPut h . encodeUtf8 $ tshow action
+    return ()
+
+extractGameAction :: World -> CoreAction.Action
+extractGameAction _ = undefined -- TODO: convert world information to action
+
+seconds :: Float -> Int 
+seconds = floor . (*1000000)
--- library/WaterWars/Core/GameAction.hs
+++ library/WaterWars/Core/GameAction.hs
@@ -5,11 +5,12 @@ import WaterWars.Core.GameState
 
 newtype Action = Action
     { rawActions :: Seq RawAction
-    }
+    } deriving (Show, Read, Eq)
 
 data RawAction
     = Run { runDirection :: RunDirection }
     | Jump
     | Shoot { shootAngle :: Angle }
+    deriving (Show, Read, Eq)
 
-data RunDirection = Left | Right
+data RunDirection = Left | Right deriving (Show, Read, Eq, Enum, Bounded)
