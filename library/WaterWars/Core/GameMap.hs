ba60500630d4541e9ce1657b0495fe858a9c8940
Samuel Pilz <e1327391@student.tuwien.ac.at>
2018-05-01 00:43:48 +0200

    move GameMap to own module & make it a semigroup

---
+++ library/WaterWars/Core/GameMap.hs
@@ -0,0 +1,33 @@
+module WaterWars.Core.GameMap where
+
+import ClassyPrelude
+import WaterWars.Core.Terrain.Block
+import Data.Array.IArray
+import WaterWars.Core.GameState
+
+
+data GameMap = GameMap
+  { gameTerrain :: Terrain
+  , gamePlayers :: Seq Player
+  }
+  deriving (Show, Read, Eq)
+
+
+-- |Terrain description of theBlockId
+data Terrain = Terrain
+  { terrainBlocks :: Array BlockLocation Block
+  , terrainBackground :: String -- TODO: how to send info about background?
+  }
+  deriving (Show, Read, Eq)
+
+
+instance Semigroup Terrain where
+    Terrain blocks1 _ <> Terrain blocks2 background =
+        Terrain (accum useSolidBlock blocks1 $ assocs blocks2) background
+        where
+            useSolidBlock :: Block -> Block -> Block
+            useSolidBlock (SolidBlock x) _ = SolidBlock x
+            useSolidBlock _ y = y
+
+newtype BlockLocation = BlockLocation (Int, Int)
+  deriving (Read, Show, Eq, Ord, Ix)
