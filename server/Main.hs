5f4a9f33390b47e1ed01faec0addda0534865d21
Samuel Pilz <samuel.pilz@posteo.net>
2018-05-16 18:24:41 +0200

    add dynamic game-file

--- server/Main.hs
+++ server/Main.hs
@@ -1,23 +1,29 @@
 module Main where
 
-import ClassyPrelude
-import Network.WebSockets
+import           ClassyPrelude
+import           Network.WebSockets
 
-import System.Log.Logger
-import System.Log.Handler.Simple
+import           System.Log.Logger
+import           System.Log.Handler.Simple
 
-import Data.UUID
-import Data.UUID.V4
+import           Data.UUID
+import           Data.UUID.V4
 
-import WaterWars.Core.DefaultGame
+import           WaterWars.Core.DefaultGame
+import           WaterWars.Core.GameMap
+import           WaterWars.Core.Terrain.Block
 
-import WaterWars.Network.Protocol as Protocol
 
-import WaterWars.Server.Config
-import WaterWars.Server.ConnectionMgnt
-import WaterWars.Server.GameLoop
-import WaterWars.Server.Client
-import WaterWars.Server.EventLoop
+import           WaterWars.Network.Protocol    as Protocol
+
+import           WaterWars.Server.Config
+import           WaterWars.Server.ConnectionMgnt
+import           WaterWars.Server.GameLoop
+import           WaterWars.Server.Client
+import           WaterWars.Server.EventLoop
+import           System.IO                                ( openFile )
+import           Data.Array.IArray
+import           Data.List                                ( transpose )
 
 defaultState :: ServerState
 defaultState = ServerState
@@ -33,6 +39,19 @@ defaultConnections =
 defaultGameSetup :: GameSetup
 defaultGameSetup = GameSetup {numberOfPlayers = 4, terrainMap = "default"}
 
+serverStateWithTerrain :: Terrain -> ServerState
+serverStateWithTerrain terrain = ServerState
+    { connections = defaultConnections
+    , gameMap     = GameMap
+        { gameTerrain       = terrain
+        , gamePlayers       = empty
+        , terrainBackground = "default"
+        }
+    , gameState   = defaultGameState
+    }
+
+-- TODO: refactor startup
+
 main :: IO ()
 main = do
   -- Copy everything to syslog from here on out.
@@ -40,9 +59,24 @@ main = do
     updateGlobalLogger rootLoggerName (addHandler s)
     updateGlobalLogger rootLoggerName (setLevel DEBUG)
 
+    -- read resources
+    terrainFile           <- openFile "resources/game1.txt" ReadMode
+    content :: ByteString <- hGetContents terrainFile
+    let terrain =
+            Terrain
+                $ listArray (BlockLocation (-8, -8), BlockLocation (8, 8))
+                . map (\c -> if c == 'x' then SolidBlock Middle else NoBlock)
+                . concat
+                . transpose
+                . reverse
+                . lines
+                . unpack
+                . decodeUtf8
+                $ content
+
     -- Initialize server state
     broadcastChan   <- atomically newBroadcastTChan
-    serverStateTvar <- newTVarIO defaultState
+    serverStateTvar <- newTVarIO $ serverStateWithTerrain terrain
 
     -- start to accept connections
     _               <- async (websocketServer serverStateTvar broadcastChan)
@@ -53,23 +87,29 @@ main = do
 websocketServer
     :: MonadIO m => TVar ServerState -> TChan (ClientMessage, Text) -> m ()
 websocketServer serverStateTvar broadcastChan =
-    liftIO $ runServer "localhost" 1234 $ \websocketConn -> do
-        connHandle <- acceptRequest websocketConn
-        debugM networkLoggerName "Client connected"
-        commChan  <- newTChanIO -- to receive messages
-        sessionId <- toText <$> nextRandom -- uniquely identify connections
-        let conn = newClientConnection sessionId
-                                       connHandle
-                                       commChan
-                                       broadcastChan
-        atomically $ do
-            ServerState {..} <- readTVar serverStateTvar
-            writeTVar
-                serverStateTvar
-                ServerState {connections = addConnection connections conn, ..}
-        clientGameThread conn
-        -- ! Should be used for cleanup code
-        return ()
+    liftIO
+        $ runServer "localhost" 1234
+        $ \websocketConn -> do
+              connHandle <- acceptRequest websocketConn
+              debugM networkLoggerName "Client connected"
+              commChan  <- newTChanIO -- to receive messages
+              sessionId <- toText <$> nextRandom -- uniquely identify connections
+              let
+                  conn = newClientConnection sessionId
+                                             connHandle
+                                             commChan
+                                             broadcastChan
+              atomically $ do
+                  ServerState {..} <- readTVar serverStateTvar
+                  writeTVar
+                      serverStateTvar
+                      ServerState
+                          { connections = addConnection connections conn
+                          , ..
+                          }
+              clientGameThread conn
+              -- ! Should be used for cleanup code
+              return ()
 
 gameLoopServer
     :: MonadIO m => TVar ServerState -> TChan (ClientMessage, Text) -> m ()
