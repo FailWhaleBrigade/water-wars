ac830bbcaba3c8247614723da56a08d2bc26682f
fendor <power.walross@gmail.com>
2019-07-13 23:05:30 +0200

    Fix shell.nix again

--- shell.nix
+++ shell.nix
@@ -7,13 +7,14 @@ stdenv.mkDerivation {
     ncurses
  
     mesa_noglu
+    libGLU_combined
     freeglut
     libpulseaudio
  ];
   src = null;
   shellHook = ''
 
-    export LD_LIBRARY_PATH=${mesa_noglu.drivers}/lib:${freeglut}/lib:${libpulseaudio}/lib:${gmp}/lib:${zlib}/lib:${ncurses}/lib
+    export LD_LIBRARY_PATH=${libGLU_combined}/lib:${mesa_noglu.drivers}/lib:${freeglut}/lib:${libpulseaudio}/lib:${gmp}/lib:${zlib}/lib:${ncurses}/lib
     export PATH=$PATH:$HOME/.local/bin
   '';
 }
