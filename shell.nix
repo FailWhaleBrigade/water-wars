43cd2baf571a1915912e021fca7df4d344ce4745
Fendor <power.walross@gmail.com>
2020-11-19 21:49:00 +0100

    Update shell nix for nixpkgs 20.03

--- shell.nix
+++ shell.nix
@@ -7,14 +7,16 @@ stdenv.mkDerivation {
     ncurses
  
     mesa_noglu
-    libGLU_combined
+    libGLU
+    libGL
+
     freeglut
     libpulseaudio
  ];
   src = null;
   shellHook = ''
 
-    export LD_LIBRARY_PATH=${libGLU_combined}/lib:${mesa_noglu.drivers}/lib:${freeglut}/lib:${libpulseaudio}/lib:${gmp}/lib:${zlib}/lib:${ncurses}/lib
+    export LD_LIBRARY_PATH=${libGL}/lib:${libGLU}/lib:${mesa_noglu.drivers}/lib:${freeglut}/lib:${libpulseaudio}/lib:${gmp}/lib:${zlib}/lib:${ncurses}/lib
     export PATH=$PATH:$HOME/.local/bin
   '';
 }
